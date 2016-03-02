cd1e66b07955bbf52504b585d57630e612673d86
Ryan Nazaretian <ryannazaretian@gmail.com>
2016-03-01 20:46:07 -0500

    Fixed more bugs with the setup script.

--- setup-gv.sh
+++ setup-gv.sh
@@ -4,15 +4,11 @@
 echo "Updating apt-get..."
 sudo apt-get update
 echo "Installing dependancies..."
-sudo apt-get install htop 
-sudo apt-get install scons
-sudo apt-get install cmake
-sudo apt-get install libjpeg8-dev
-sudo apt-get install python-pip
-sudo apt-get install oracle-java8-jdk
+sudo apt-get install htop scons cmake libjpeg8-dev python-pip oracle-java8-jdk
 sudo pip install pyro
 
 # Install mjpg-streamer
+echo "Installing mjpg-streamer"
 cd vision
 git clone https://github.com/jacksonliam/mjpg-streamer.git
 cd mjpg-streamer/mjpg-streamer-experimental
@@ -20,9 +16,10 @@ sudo make clean
 sudo make install
 
 # Install WS2812B Python Driver
+echo "Installing WS2812B Python Driver"
 cd ../../..
 git clone https://github.com/popoklopsi/rpi_ws281x.git
-cd rpi_ws281x.git
+cd rpi_ws281x 
 sudo scons
 
 # Make shell scripts executable
