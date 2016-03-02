a65295d56ab40b73f0e19ec3125e969f600025b7
Ryan Nazaretian <ryannazaretian@gmail.com>
2016-03-01 22:53:07 -0500

    Updates for the gv service.

--- setup-gv.sh
+++ setup-gv.sh
@@ -26,7 +26,7 @@ sudo python setup.py install
 cd ~/
 chmod 755 *.sh
 
-echo "./gv-service.py &" >> ~/.profile
+echo "~/gv-service.sh &" >> ~/.profile
 
 
 
--- stop_all.sh
+++ stop_all.sh
@@ -3,4 +3,5 @@ sudo killall mjpg_streamer
 sudo killall python
 sudo killall pyro4-ns
 sudo python /home/pi/leds/set_leds.py 0 0 0
+sudo killall bash
 
