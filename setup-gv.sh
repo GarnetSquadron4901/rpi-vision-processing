848861051042ec7b360c2fdcfde4538a3f6a7eff
Ryan Nazaretian <ryannazaretian@gmail.com>
2016-03-01 20:23:52 -0500

    Updated the install script

--- setup-gv.sh
+++ setup-gv.sh
@@ -1,17 +1,26 @@
 # Get dependacies
 sudo apt-get update
-sudo apt-get install htop scons cmake libjpeg8-dev oracle-java8-jdk
+sudo apt-get install htop scons cmake libjpeg8-dev python-pip oracle-java8-jdk
+sudo pip install pyro
 
-# Install mjpg-straemer
-mkdir ~/vision
-cd ~vision
+# Install mjpg-streamer
+mkdir vision
+cd vision
 git clone https://github.com/jacksonliam/mjpg-streamer.git
+cd mjpg-streamer/mjpg-streamer-experimental
+sudo make clean
+sudo make install
 
 # Install WS2812B Python Driver
-cd ~/
+cd ..
 git clone https://github.com/popoklopsi/rpi_ws281x.git
+cd rpi_ws281x.git
+sudo scons
 
-# Install GRIP dependacies
-mkdir ~/vision/grip
+# Make shell scripts executable
+cd ..
+chmod 755 *.sh
+
+
 
 
