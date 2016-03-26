4d545681cf884f421a4bd7badb4bc95ae5de1e27
Ryan Nazaretian <ryannazaretian@gmail.com>
2016-03-26 14:29:19 -0400

    Try 2

--- gv-service.sh
+++ gv-service.sh
@@ -1,4 +1,4 @@
-if [ps -a | grep pyro4-ns] || [ps -a | grep mjpg-streamer] || [ps -a | grep java] || [ps -a | grep python]; then
+if (ps -a | grep pyro4-ns) || (ps -a | grep mjpg-streamer) || (ps -a | grep java) || (ps -a | grep python); then
 	echo Garnet Vision process already running. Aborting.
 	exit
 fi
